98458b0b5e494b125c42912bb03ff6df472ff59e
Victor Emanuel Almeida <victoralmeida2001@hotmail.com>
2021-09-08 13:17:52 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,11 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
     Grafo g;
-    string arq;
     if (argc == 2) {
-        arq = argv[1];
-        g.ler(arq);
+        g.ler(argv[1]);
     }
-    /*
-    g.buscaEmLargura(3);
-    g.buscaEmProfundidade(3);
-    g.bellmanFord(3);
-    g.kruskal();
-    */
     MenuPrincipal menu (&g);
     menu.loop();
+
     return 0;
 }
